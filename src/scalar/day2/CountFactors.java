649b2576ccd5f270f675bab6b9c91f1f9ed922bf
NikhilKumarNK5 <nikhil.nikki.nk05@gmail.com>
2025-07-10 21:01:22 +0530

    countFactors

---
+++ src/scalar/day2/CountFactors.java
@@ -0,0 +1,34 @@
+package scalar.day2;
+
+/*
+Problem Description
+Given an integer A, you need to find the count of it's factors.
+Factor of a number is the number which divides it perfectly leaving no remainder.
+Example : 1, 2, 3, 6 are factors of 6
+
+Problem Constraints
+1 <= A <= 109
+
+Input Format
+First and only argument is an integer A.
+
+Output Format
+Return the count of factors of A.
+*/
+
+public class CountFactors {
+    public int solve(int A) {
+        int count = 0;
+        for(int i = 1; i * i <= A; i++) {
+            if(A % i == 0) {
+                if(i != A/i) {
+                    count += 2;
+                }
+                else {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
