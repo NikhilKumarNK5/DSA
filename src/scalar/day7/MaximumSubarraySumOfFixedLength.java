79f7e14deb6046c4882e888a6eed954b0e608e99
NikhilKumarNK5 <nikhil.nikki.nk05@gmail.com>
2025-07-13 22:46:05 +0530

    MaximumSubarraySumOfFixedLength

--- src/scalar/day7/MaximumSubarraySumOfFixedLength.java
+++ src/scalar/day7/MaximumSubarraySumOfFixedLength.java
@@ -1,5 +1,32 @@
 package scalar.day7;
 
+/*
+Problem Description
+Given an array A of length N, Find the maximum subarray sum out of all possible subarray of length B.
+
+Problem Constraints
+1 <= N <= 105
+1 <= A[i] <= 106
+
+Input Format
+First argument A is an array of integers.
+Second argument B is length of subarray.
+
+Output Format
+Return a single integer i.e. max subarray sum
+
+Input 1:
+A = [6, 7, 8, 2]
+B = 2
+Input 2:
+A = [3, 9, 5, 6, 5, 11]
+B = 3
+
+Output 1:
+15
+Output 2:
+22
+*/
 public class MaximumSubarraySumOfFixedLength {
     public long solve(int[] A, int B) {
         long ans = 0;
